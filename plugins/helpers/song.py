2cf0215bd53fe7a3006ed4e9e768bccba61b867f
Jimmysbot <119768673+Jimmysbot@users.noreply.github.com>
2023-01-28 19:13:56 +0530

    Update song.py

--- plugins/helpers/song.py
+++ plugins/helpers/song.py
@@ -66,7 +66,7 @@ def song(client, message):
             info_dict = ydl.extract_info(link, download=False)
             audio_file = ydl.prepare_filename(info_dict)
             ydl.process_info(info_dict)
-        rep = '<b> <a href=https://t.me/mallu_music_group>Mᴀʟʟᴜ Mᴜsɪᴄ ✨</a> </b>'
+        rep = '[Mᴀʟʟᴜ | Mᴜsɪᴄ ✨ ](https://t.me/mallu_music_group)'
         secmul, dur, dur_arr = 1, 0, duration.split(':')
         for i in range(len(dur_arr)-1, -1, -1):
             dur += (int(dur_arr[i]) * secmul)
